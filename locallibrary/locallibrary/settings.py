e89a5b041c2b403bd05cafa04163fd17d8c5a628
belladianamiller <isabelladianamiller@gmail.com>
2025-04-09 20:13:32 -0400

    Update settings.py

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -21,9 +21,6 @@ env = environ.Env()
 # Load environment variables
 env.read_env(str(BASE_DIR / ".env"))
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 
